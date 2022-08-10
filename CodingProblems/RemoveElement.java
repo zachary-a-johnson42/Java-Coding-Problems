76b2f1a59e8f78c9a6bed099776cebae36614962
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-08-10 10:58:45 -0500

    not the cleanest solution. Maybe come back and refactor

--- CodingProblems/RemoveElement.java
+++ CodingProblems/RemoveElement.java
@@ -1,34 +1,16 @@
 package CodingProblems;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+//https://leetcode.com/problems/remove-element/
 
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        // converting array into list to use iterator
-        List numsList = IntStream.of(nums).boxed().collect(Collectors.toList());
-        // setting up iterator for removal
-        Iterator<Integer> numsIter = numsList.iterator();
-        //setting up a list to catch what the iterator doesn't get rid of
-        List returnList = new ArrayList<Integer>();
-        // using while loop to go through with iterator and remove elements
         int counter = 0;
-        while(numsIter.hasNext()) {
-            Integer numToRemove = numsIter.next();
-            if ( numToRemove == val){
-                numsIter.remove();
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[counter] = nums[i];
                 counter++;
             }
-            if(numToRemove != val) {
-                returnList.add(numToRemove);
-            }
         }
-        int[] returnArray = returnList.stream().mapToInt(i -> (int) i).toArray();
         return counter;
     }
 }
