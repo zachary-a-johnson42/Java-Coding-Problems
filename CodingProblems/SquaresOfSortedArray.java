0e84057b31e0db678e8a207904ed586205dbad06
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-08-15 14:40:37 -0500

    Solved SquaresOfSortedArray

---
+++ CodingProblems/SquaresOfSortedArray.java
@@ -0,0 +1,23 @@
+package CodingProblems;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SquaresOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int[] returnArray = new int[nums.length];
+        // squaring everything in the nums array
+        for (int i = 0; i < nums.length; i++) {
+            returnArray[i] = nums[i] * nums[i];
+        }
+
+        //creating intStream
+        Stream<Integer> returnStream = Arrays.stream(returnArray).boxed();
+        //Sorting
+        returnArray = returnStream.mapToInt(item -> item).sorted().toArray();
+
+        return returnArray;
+    }
+}
